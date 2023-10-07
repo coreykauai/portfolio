8f19c9b09f1ba6c3938148f48acda15c8a437a06
Coreykauai <corey@student.dev-island.org>
2023-10-07 09:16:42 -1000

    Portfolio first push

---
+++ script.js
@@ -0,0 +1,40 @@
+let click1 = document.querySelector(".numone");
+let click2 = document.querySelector(".numtwo");
+let click3 = document.querySelector(".numthree");
+let click4 = document.querySelector(".numfour");
+let click5 = document.querySelector(".numfive");
+let click6 = document.querySelector(".numsix");
+let linkd = document.querySelector(".linklink");
+let linkk = document.querySelector(".linklinkk");
+
+click1.addEventListener("click", function (event) {
+  window.location.href = "pics/Signupform.png";
+});
+
+click2.addEventListener("click", function (event) {
+  window.location.href = "pics/rockpaperscissors.png";
+});
+
+click3.addEventListener("click", function (event) {
+  window.location.href = "https://verbose-adventure-n15ey45.pages.github.io/";
+});
+
+click4.addEventListener("click", function (event) {
+  window.location.href = "pics/etchit.png";
+});
+
+click5.addEventListener("click", function (event) {
+  window.location.href = "pics/calculator.png";
+});
+
+click6.addEventListener("click", function (event) {
+  window.location.href = "https://mamatiffncompany.com/";
+});
+
+linkd.addEventListener("click", function (event) {
+  window.location.href = "www.linkedin.com/in/corey-sugai";
+});
+
+linkk.addEventListener("click", function (event) {
+  window.location.href = "www.linkedin.com/in/corey-sugai";
+});
